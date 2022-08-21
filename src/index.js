25ddc3bae7b0155dcd774fd768f26585451c3997
manish kumar <manish927sss@gmil.com>
2022-08-21 16:28:04 +0530

    added navbar

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import NavBar from "./nav/nav.jsx";
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const webPage = 
+    <>
+      <App />
+      <NavBar /> 
+    </>
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(webPage);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/nav/nav.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+import "./nav.css";
+
+function NavBar() {
+
+  return  <div style={{topMargin: "60px"}}>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <ul class="topnav">
+                <li><a class="active" href="#home">Home</a></li>
+                <li><a href="#news">News</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li class="right"><a href="#about">About</a></li>
+            </ul>
+    </div> 
+}
+
+export default NavBar;
