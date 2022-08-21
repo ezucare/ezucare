c320f03d82dfe6c60545563056f399f92286df58
manish kumar <manish927sss@gmil.com>
2022-08-21 16:53:20 +0530

    class to className

--- src/nav/nav.jsx
+++ src/nav/nav.jsx
@@ -4,11 +4,11 @@ function NavBar() {
 
   return  <div style={{topMargin: "60px"}}>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <ul class="topnav">
-                <li><a class="active" href="#home">Home</a></li>
+            <ul className="topnav">
+                <li><a className="active" href="#home">Home</a></li>
                 <li><a href="#news">News</a></li>
                 <li><a href="#contact">Contact</a></li>
-                <li class="right"><a href="#about">About</a></li>
+                <li className="right"><a href="#about">About</a></li>
             </ul>
     </div> 
 }
